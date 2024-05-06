1f5b53c4863f894f39f3f6696e6bfb5858155b64
Cristian Flores <cristian.jf@outlook.com>
2024-05-05 23:11:17 -0600

    Open boundary implemented to generate a free stream at the bulk of the domain.

--- test/cylinder_test.cpp
+++ test/cylinder_test.cpp
@@ -1,9 +1,6 @@
-#include <ATen/ops/zeros_like.h>
-#include <c10/core/DeviceType.h>
+#include <ATen/TensorIndexing.h>
 #include <iostream>
 #include <ostream>
-#include <torch/csrc/autograd/generated/variable_factories.h>
-#include <torch/cuda.h>
 #include <torch/torch.h>
 #include <toml++/toml.hpp>
 #include "../src/params.hpp"
@@ -15,23 +12,10 @@ using torch::indexing::Slice;
 using torch::indexing::Ellipsis;
 using std::cout;
 using std::cerr;
+
 using utils::indices;
-
-// Boundaries
-const indices virtual_inlet{0, Ellipsis};
-const indices inlet{1, Ellipsis};
-const indices outlet{-2, Ellipsis};
-const indices virtual_outlet{-1, Ellipsis};
-
-void periodic_boundary_condition
-(
-  torch::Tensor& f_coll,
-  const torch::Tensor& f_equi,
-  const torch::Tensor& u,
-  const torch::Tensor& rho,
-  const double rho_inlet,
-  const double rho_outlet
-);
+using solver::E;
+using solver::c;
 
 int main(int argc, char* argv[])
 {
@@ -50,19 +34,7 @@ int main(int argc, char* argv[])
   cout << lp << "\n";
   const params::simulation sp{tbl, lp};
   cout << sp << "\n";
-  // Parameters for the current study case
-  // Hagen-Poiseuille pressure drop
-  const double p_grad = 8.0*lp.nu*lp.u/(lp.Y*lp.Y);
-  const double rho_outlet = 1.0;
-  const double rho_inlet = 3.0*(lp.X - 1)*p_grad + rho_outlet;
-  utils::print("p_grad", p_grad);
-  utils::print("rho_outlet", rho_outlet);
-  utils::print("rho_inlet", rho_inlet);
 
-  if (argc < 3)
-    if(!utils::continue_execution()) return 0;
-  if (argc >= 3 && argv[2][0] == 'a')
-    return 0;
 
   torch::set_default_dtype(caffe2::scalarTypeToTypeMeta(torch::kDouble));
   if (!torch::cuda::is_available())
@@ -76,12 +48,27 @@ int main(int argc, char* argv[])
   Tensor f_coll = torch::zeros_like(f_equi, dev);
   Tensor f_adve = torch::zeros_like(f_equi, dev);
   Tensor u = torch::zeros({lp.X, lp.Y, 2}, dev);
-  Tensor rho = torch::zeros({lp.X, lp.Y, 1}, dev);
+  Tensor rho = torch::ones({lp.X, lp.Y, 1}, dev);
 
   // Results
   Tensor ux = torch::zeros({lp.X, lp.Y, sp.total_snapshots});
   Tensor uy = torch::zeros_like(ux);
   Tensor rhos = torch::zeros_like(ux);
+
+  // Parameters for the current study case
+  const double p_grad = 8.0*lp.nu*lp.u/(lp.Y*lp.Y);
+  const double rho_outlet = 1.0;
+  const double rho_inlet = 1.6; //3.0*(lp.X-1)*p_grad + rho_outlet;
+  utils::print("p_grad", p_grad);
+  utils::print("rho_outlet", rho_outlet);
+  utils::print("rho_inlet", rho_inlet);
+  Tensor u_w = torch::zeros({lp.Y, 2}, dev);
+  Tensor abb_bc = torch::zeros({lp.Y, 1}, dev);
+
+  if (argc < 3)
+    if(!utils::continue_execution()) return 0;
+  if (argc >= 3 && argv[2][0] == 'a')
+    return 0;
 
   // Initialization
   solver::incomp_equilibrium(f_adve, u, rho);
@@ -108,20 +95,41 @@ int main(int argc, char* argv[])
     // Collision, BGK operator
     solver::collision(f_coll, f_adve, f_equi, lp.omega);
 
-    // Inlet and outlet periodic boundary conditions
-    periodic_boundary_condition(f_coll, f_equi, u, rho, rho_inlet, rho_outlet);
-
     // Advection
     solver::advect(f_adve, f_coll);
 
-    // No slip boundary conditions for the first and last column
+    // Anti-bounce-back boundary conditions
+    // Inlet
+    u_w = (1.5*u.index({0, Ellipsis}) - 0.5*u.index({1, Ellipsis})).clone().detach();
+    abb_bc = ((2.0 + 9.0*torch::pow(u_w.matmul(c),2.0) - 3.0*u_w.mul(u_w).sum(1).unsqueeze(1))*E*rho_inlet).squeeze(0).clone().detach();
+    f_adve.index({0, Slice(), 3}) = (-f_coll.index({0, Slice(), 1}) + abb_bc.index({Slice(), 1})).clone().detach();
+    f_adve.index({0, Slice(), 4}) = (-f_coll.index({0, Slice(), 2}) + abb_bc.index({Slice(), 2})).clone().detach();
+    f_adve.index({0, Slice(), 1}) = (-f_coll.index({0, Slice(), 3}) + abb_bc.index({Slice(), 3})).clone().detach();
+    f_adve.index({0, Slice(), 2}) = (-f_coll.index({0, Slice(), 4}) + abb_bc.index({Slice(), 4})).clone().detach();
+    f_adve.index({0, Slice(), 7}) = (-f_coll.index({0, Slice(), 5}) + abb_bc.index({Slice(), 5})).clone().detach();
+    f_adve.index({0, Slice(), 8}) = (-f_coll.index({0, Slice(), 6}) + abb_bc.index({Slice(), 6})).clone().detach();
+    f_adve.index({0, Slice(), 5}) = (-f_coll.index({0, Slice(), 7}) + abb_bc.index({Slice(), 7})).clone().detach();
+    f_adve.index({0, Slice(), 6}) = (-f_coll.index({0, Slice(), 8}) + abb_bc.index({Slice(), 8})).clone().detach();
+    // Outlet
+    u_w = (1.5*u.index({-1, Ellipsis}) - 0.5*u.index({-2, Ellipsis})).clone().detach();
+    abb_bc = ((2.0 + 9.0*torch::pow(u_w.matmul(c),2.0) - 3.0*u_w.mul(u_w).sum(1).unsqueeze(1))*E).squeeze(0).clone().detach();
+    f_adve.index({-1, Slice(), 3}) = (-f_coll.index({-1, Slice(), 1}) + abb_bc.index({Slice(), 1})).clone().detach();
+    f_adve.index({-1, Slice(), 4}) = (-f_coll.index({-1, Slice(), 2}) + abb_bc.index({Slice(), 2})).clone().detach();
+    f_adve.index({-1, Slice(), 1}) = (-f_coll.index({-1, Slice(), 3}) + abb_bc.index({Slice(), 3})).clone().detach();
+    f_adve.index({-1, Slice(), 2}) = (-f_coll.index({-1, Slice(), 4}) + abb_bc.index({Slice(), 4})).clone().detach();
+    f_adve.index({-1, Slice(), 7}) = (-f_coll.index({-1, Slice(), 5}) + abb_bc.index({Slice(), 5})).clone().detach();
+    f_adve.index({-1, Slice(), 8}) = (-f_coll.index({-1, Slice(), 6}) + abb_bc.index({Slice(), 6})).clone().detach();
+    f_adve.index({-1, Slice(), 5}) = (-f_coll.index({-1, Slice(), 7}) + abb_bc.index({Slice(), 7})).clone().detach();
+    f_adve.index({-1, Slice(), 6}) = (-f_coll.index({-1, Slice(), 8}) + abb_bc.index({Slice(), 8})).clone().detach();
+
+    // Specular boundary conditions
     f_adve.index({Slice(), -1, 4}) = f_coll.index({Slice(), -1, 2}).clone().detach();
-    f_adve.index({Slice(), -1, 7}) = f_coll.index({Slice(), -1, 5}).clone().detach();
-    f_adve.index({Slice(), -1, 8}) = f_coll.index({Slice(), -1, 6}).clone().detach();
+    f_adve.index({Slice(), -1, 7}) = f_coll.index({Slice(), -1, 6}).clone().detach();
+    f_adve.index({Slice(), -1, 8}) = f_coll.index({Slice(), -1, 5}).clone().detach();
 
     f_adve.index({Slice(), 0, 2}) = f_coll.index({Slice(), 0, 4}).clone().detach();
-    f_adve.index({Slice(), 0, 5}) = f_coll.index({Slice(), 0, 7}).clone().detach();
-    f_adve.index({Slice(), 0, 6}) = f_coll.index({Slice(), 0, 8}).clone().detach();
+    f_adve.index({Slice(), 0, 5}) = f_coll.index({Slice(), 0, 8}).clone().detach();
+    f_adve.index({Slice(), 0, 6}) = f_coll.index({Slice(), 0, 7}).clone().detach();
   }
 
   // Save results
@@ -131,26 +139,4 @@ int main(int argc, char* argv[])
   torch::save(rhos/3.0, sp.file_prefix + "ct-ps.pt");
 
   return 0;
-}
-
-void periodic_boundary_condition
-(
-  torch::Tensor& f_coll,
-  const torch::Tensor& f_equi,
-  const torch::Tensor& u,
-  const torch::Tensor& rho,
-  const double rho_inlet,
-  const double rho_outlet
-)
-{
-  torch::Tensor temp_equi = torch::zeros({1, u.sizes()[1], 9}, torch::kCUDA);
-  torch::Tensor temp_rho = torch::ones({1, u.sizes()[1], 1}, torch::kCUDA);
-
-  // inlet
-  solver::incomp_equilibrium(temp_equi, u.index(outlet).unsqueeze(0), rho_inlet*temp_rho);
-  f_coll.index(virtual_inlet) = (temp_equi + f_coll.index(outlet) - f_equi.index(outlet)).squeeze(0).clone().detach();
-
-  // outlet
-  solver::incomp_equilibrium(temp_equi, u.index(inlet).unsqueeze(0), rho_outlet*temp_rho);
-  f_coll.index(virtual_outlet) = (temp_equi + f_coll.index(inlet) - f_equi.index(inlet)).squeeze(0).clone().detach();
 }
