6c0f888bdee18f7a02ffe257adb662cda7cd4db2
Cristian Flores <cristian.jf@outlook.com>
2024-05-05 23:21:42 -0600

    Set print function as template, add optional ending character.

--- src/utils.hpp
+++ src/utils.hpp
@@ -13,12 +13,16 @@ typedef std::initializer_list<at::indexing::TensorIndex> indices;
 
 void store_tensor(const torch::Tensor& source, torch::Tensor& target, int i);
 
-void print(const std::string& str);
+template<typename T>
+void print(const T& t, const char& end='\n')
+{
+  std::cout << t << end;
+}
 
 template<typename T>
-void print(const std::string& name, const T& t)
+void print(const std::string& name, const T& t, const char& end='\n')
 {
-  std::cout << name << "=" << t << std::endl;
+  std::cout << name << "=" << t << end;
 }
 }
 
